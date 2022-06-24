d3c7bdf5d0b4f18152a296d3881225420ad5347c
Muhammad Zeeshan <zeeshan.m@datum.lahore>
2022-06-24 22:03:04 +0500

    initial commit

---
+++ classes/fileSystem.php
@@ -0,0 +1,29 @@
+<?php
+
+class FileSystem
+{
+    /*
+        This method opens the file from the given path and returns the array of strings
+    */
+    public function fileToArray($path)
+    {
+        $file_open  = fopen($path, "r") or die("Unable to open file!");
+        $file_data  = fread($file_open, filesize($path));
+        $file_array = explode(PHP_EOL, $file_data);
+        fclose($file_open);
+        return $file_array;
+    }
+
+    /*
+        This method Takes two arrays, compares them, and gives the difference between the two arrays.
+        A new file will be created in the downloads folder which will have the difference of two arrays
+        and saves with the name given as the third argument in the method.
+    */
+    public function saveDiffernceFile($first_array, $second_array, $file_name = 'file')
+    {
+        $arr_difference = array_diff($first_array, $second_array);
+        $file_write = fopen("downloads/" . $file_name . ".txt", "w") or die("Unable to open file!");
+        $implode_array =  implode(PHP_EOL, $arr_difference);
+        fwrite($file_write, $implode_array);
+    }
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once "classes/fileSystem.php";
+
+$fileSystem = new FileSystem;
+
+// Passing the files to method to get the strings in the form of arrays
+$first_file_array = $fileSystem->fileToArray("files/first.txt");
+$second_file_array = $fileSystem->fileToArray("files/second.txt");
+
+// Using the method from the FileSystem class to compare two files and save the resulted files in downloads folder.
+$fileSystem->saveDiffernceFile($first_file_array, $second_file_array, 'first_file');
+$fileSystem->saveDiffernceFile($second_file_array, $first_file_array, 'second_file');
+
+echo 'Files Saved in Downloads folder';
+
+exit;
